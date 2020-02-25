ada13134145dbdab679409fd260597e19d29bcd2
bobbyrhmn-apps <bobbyrhmn.apps@gmail.com>
2020-02-25 23:34:11 +0700

    Membuat Notification

--- app/src/main/java/com/bobbyrhmn/movapps/checkout/CheckoutActivity.kt
+++ app/src/main/java/com/bobbyrhmn/movapps/checkout/CheckoutActivity.kt
@@ -1,9 +1,17 @@
 package com.bobbyrhmn.movapps.checkout
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.app.NotificationCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bobbyrhmn.movapps.R
 import com.bobbyrhmn.movapps.checkout.adapter.CheckoutAdapter
@@ -17,9 +25,9 @@ import kotlin.collections.ArrayList
 class CheckoutActivity : AppCompatActivity() {
 
     private var dataList = ArrayList<Checkout>()
-    private var total:Int = 0
+    private var total: Int = 0
 
-    private lateinit var preferences:Preferences
+    private lateinit var preferences: Preferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,23 +37,30 @@ class CheckoutActivity : AppCompatActivity() {
         dataList = intent.getSerializableExtra("data") as ArrayList<Checkout>
 
 
-        for (a in dataList.indices){
+        for (a in dataList.indices) {
             total += dataList[a].harga!!.toInt()
         }
 
         dataList.add(Checkout("Total Harus Dibayar", total.toString()))
 
         btn_tiket.setOnClickListener {
-            val intent = Intent(this@CheckoutActivity,
-                CheckoutSuccessActivity::class.java)
+            val intent = Intent(
+                this@CheckoutActivity,
+                CheckoutSuccessActivity::class.java
+            )
             startActivity(intent)
+
+            showNotif()
+        }
+        btn_home.setOnClickListener {
+            finish()
         }
 
         rc_checkout.layoutManager = LinearLayoutManager(this)
         rc_checkout.adapter = CheckoutAdapter(dataList) {
         }
 
-        if(preferences.getValues("saldo")!!.isNotEmpty()) {
+        if (preferences.getValues("saldo")!!.isNotEmpty()) {
             val localeID = Locale("in", "ID")
             val formatRupiah = NumberFormat.getCurrencyInstance(localeID)
             tv_saldo.setText(formatRupiah.format(preferences.getValues("saldo")!!.toDouble()))
@@ -57,5 +72,48 @@ class CheckoutActivity : AppCompatActivity() {
             textView3.visibility = View.VISIBLE
             textView3.text = "Saldo pada E-Wallet anda tidak mencukupi untuk melakukan transaksi"
         }
+    }
+
+    private fun showNotif() {
+        val NOTIFICATION_CHANNEL_ID = "channel_bwa_notif"
+        val context = this.applicationContext
+        var notificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            val channelName = "BWAMOV Notif Channel"
+            val importance = NotificationManager.IMPORTANCE_HIGH
+
+            val mChannel= NotificationChannel(NOTIFICATION_CHANNEL_ID,channelName,importance)
+            notificationManager.createNotificationChannel(mChannel)
+        }
+
+        val mIntent = Intent (this, CheckoutSuccessActivity::class.java)
+        val bundle = Bundle()
+        bundle.putString("id", "id_film")
+        mIntent.putExtras(bundle)
+
+        val pendingIntent =
+            PendingIntent.getActivity(this, 0,mIntent, PendingIntent.FLAG_CANCEL_CURRENT)
+
+        val builder = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+        builder.setContentIntent(pendingIntent)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setLargeIcon(
+                BitmapFactory.decodeResource(
+                    this.resources,
+                    R.mipmap.ic_launcher
+                )
+            )
+            .setTicker("Notif MOV Apps Starting")
+            .setAutoCancel(true)
+            .setVibrate(longArrayOf(1000, 1000, 1000, 1000, 1000))
+            .setLights(Color.RED, 3000, 3000)
+            .setDefaults(Notification.DEFAULT_SOUND)
+            .setContentTitle("Pembayaran Berhasil")
+            .setContentText("MOV APPS")
+
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(115, builder.build())
     }
 }
